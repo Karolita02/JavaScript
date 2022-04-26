e6d95f038c701812ae6f7232548415059370b286
Thais C. Samudio A <samudioarauz@gmail.com>
2022-04-26 10:43:54 -0500

    Aprendiendo las bases de JavaScript

---
+++ Codigo.js
@@ -0,0 +1,14 @@
+//console.log  mostrar mensaje en consola
+ // alert mostrar mensaje como de alerta en una pequeña ventana o algo asi
+// crear variables
+variable = 10;
+console.log(variable);
+variable = "hola";
+// <br/> salto de linea 
+
+// para usar funciones debemos colocar la palabra function seguido del nombre que queramos
+// igual podemos utilizar return y devolver lo que queramos
+// prompt() es para leer entrada del usuario 
+// confirm() cuadro de confirmacion
+// para crear arreglos a = new Array(tamaño) o a = [1,2,3]
+// 
